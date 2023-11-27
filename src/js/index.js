cb942edbaecf2651afbb236897555c0e04369607
Igor Bodrov <bodrovigor02@gmail.com>
2023-11-27 14:53:00 +0300

    feat: 🎸 Разметка, стилизация

--- src/js/index.js
+++ src/js/index.js
@@ -11,9 +11,7 @@ function randNum(min, max) {
 // ? --- Отключение подсветки ошибок в инпутах
 window.addEventListener('load', () => {
   if (!(document.querySelectorAll('input') === null)) {
-    document.querySelectorAll('input').forEach((input) => {
-      input.setAttribute('spellcheck', 'false');
-    })
+    document.querySelectorAll('input').forEach((input) => input.setAttribute('spellcheck', 'false'));
   }
 })
 
@@ -23,18 +21,42 @@ accordionFunc('services');
 
 // ? --- Звёзды рейтинга
 window.addEventListener('load', () => {
-  let shopItems = document.querySelectorAll('.reviews__slider-slide');
-  console.log(shopItems);
+  if (ifExist('reviews__slider-slide')) {
+    let reviewItems = document.querySelectorAll('.reviews__slider-slide');
 
-  if (shopItems === null) {
-    return;
-  }
-  else {
-    shopItems.forEach(item => {
-      let shopItemsStars = item.querySelectorAll('.reviews__slider-slide-star');
+    reviewItems.forEach(item => {
+      let reviewItemsStars = item.querySelectorAll('.reviews__slider-slide-star');
       for (let i = 0; i < item.dataset.rating; i++) {
-        Array.from(shopItemsStars)[i].classList.add('reviews__slider-slide-star--active');
+        Array.from(reviewItemsStars)[i].classList.add('reviews__slider-slide-star--active');
       }
     })
+  }
+});
+
+// ? --- Обрезание текста ревью
+window.addEventListener('load', () => {
+  if (ifExist('reviews__slider-slide-text')) {
+    let reviewItemText = document.querySelectorAll('.reviews__slider-slide-text');
+    reviewItemText.forEach(item => {
+      if (item.scrollHeight > item.clientHeight) {
+        item.classList.add('reviews__slider-slide-text--clamped');
+        item.addEventListener('click', () => { item.classList.toggle('reviews__slider-slide-text--active') })
+      }
+    })
+  }
+});
+
+// ? --- Фикс анимации в блоке услуг
+window.addEventListener('load', () => {
+  if (ifExist('all-services__item')) {
+    let allServicesItems = document.querySelectorAll('.all-services__item');
+    allServicesItems.forEach(item => item.addEventListener('mouseover', () => {
+      item.classList.add('all-services__item--hovered');
+    }))
+    allServicesItems.forEach(item => item.addEventListener('mouseleave', () => {
+      setTimeout(() => {
+        item.classList.remove('all-services__item--hovered');
+      }, 120)
+    }))
   }
 });
